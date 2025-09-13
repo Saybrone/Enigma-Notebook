0a809593ad0e5a5046cbee58e31c74c505c2e218
saybrone <batupekcan9q@gmail.com>
2025-09-13 21:56:21 +0300

    Update Enigma-Notebook.py

--- Enigma-Notebook.py
+++ Enigma-Notebook.py
@@ -4,6 +4,7 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
 import curses
 import sys
+import copy
 
 
 def editor(stdscr, path=None):
@@ -13,6 +14,15 @@ def editor(stdscr, path=None):
     max_y, max_x = stdscr.getmaxyx()
     text = []
     cursor_y, cursor_x = 0, 0
+    scroll_offset = 0
+    undo_stack = []
+    redo_stack = []
+    def snapshot():
+        return (copy.deepcopy(text), cursor_y, cursor_x, scroll_offset)
+
+    def restore(state):
+        nonlocal text, cursor_y, cursor_x, scroll_offset
+        text, cursor_y, cursor_x, scroll_offset = copy.deepcopy(state[0]), state[1], state[2], state[3]
 
     try:
         if fernet is not None and path is not None:
@@ -29,26 +39,31 @@ def editor(stdscr, path=None):
 
         while True:
             stdscr.clear()
-            for i, line in enumerate(text):
-                if i >= max_y - 1:
+            for i in range(max_y - 1):
+                line_index = scroll_offset + i
+                if line_index >= len(text):
                     break
-                stdscr.addstr(i, 0, line[:max_x-1])
-            display = f"F1 Save | F2 Quit | Ln {cursor_y+1}, Col {cursor_x+1}"
+                stdscr.addstr(i, 0, text[line_index][:max_x-1])
+            display = f"F1 Save | F2 Quit |F3 Undo | F4 Redo | Ln {cursor_y+1}, Col {cursor_x+1}"
             stdscr.addstr(max_y-1, 0, display[:max_x-1], curses.A_REVERSE)
-            stdscr.move(cursor_y, cursor_x)
+            screen_y = cursor_y - scroll_offset
+            if 0 <= screen_y < max_y-1:
+                stdscr.move(screen_y, min(cursor_x, max_x-2))
             stdscr.refresh()
 
             key = stdscr.getch()
 
             if key == curses.KEY_UP:
-                if cursor_y > 0:
-                    cursor_y -= 1
+                cursor_y = max(0, cursor_y-1)
                 cursor_x = min(cursor_x, len(text[cursor_y]))
+                if cursor_y < scroll_offset:
+                    scroll_offset -= 1
 
             elif key == curses.KEY_DOWN:
-                if cursor_y < len(text)-1:
-                    cursor_y += 1
+                cursor_y = min(len(text)-1, cursor_y+1)
                 cursor_x = min(cursor_x, len(text[cursor_y]))
+                if cursor_y - scroll_offset >= max_y - 1:
+                    scroll_offset += 1
 
             elif key == curses.KEY_LEFT:
                 if cursor_x > 0:
@@ -56,6 +71,8 @@ def editor(stdscr, path=None):
                 elif cursor_y > 0:
                     cursor_y -= 1
                     cursor_x = len(text[cursor_y])
+                    if cursor_y < scroll_offset:
+                        scroll_offset -= 1
 
             elif key == curses.KEY_RIGHT:
                 if cursor_x < len(text[cursor_y]):
@@ -63,15 +80,24 @@ def editor(stdscr, path=None):
                 elif cursor_y < len(text)-1:
                     cursor_y += 1
                     cursor_x = 0
+                    if cursor_y - scroll_offset >= max_y - 1:
+                        scroll_offset += 1
 
             elif key in (10, 13):
+                undo_stack.append(snapshot())
+                redo_stack.clear()
                 rest = text[cursor_y][cursor_x:]
                 text[cursor_y] = text[cursor_y][:cursor_x]
                 text.insert(cursor_y+1, rest)
                 cursor_y += 1
                 cursor_x = 0
+                if cursor_y - scroll_offset >= max_y - 1:
+                    scroll_offset += 1
 
             elif key in (curses.KEY_BACKSPACE, 127, 8):
+                if cursor_x > 0 or cursor_y > 0:
+                    undo_stack.append(snapshot())
+                    redo_stack.clear()
                 if cursor_x > 0:
                     text[cursor_y] = text[cursor_y][:cursor_x-1] + text[cursor_y][cursor_x:]
                     cursor_x -= 1
@@ -81,8 +107,10 @@ def editor(stdscr, path=None):
                     del text[cursor_y]
                     cursor_y -= 1
                     cursor_x = prev_len
+                    if cursor_y < scroll_offset:
+                        scroll_offset -= 1
 
-            elif key == curses.KEY_F1: 
+            elif key == curses.KEY_F1:
                 if path:
                     with open(path, "w") as fh:
                         writetext = "\n".join(text)
@@ -99,7 +127,7 @@ def editor(stdscr, path=None):
                             writetext = "\n".join(text)
                             fh.write(fernet.encrypt(writetext.encode()).decode())
 
-            elif key == curses.KEY_F2: 
+            elif key == curses.KEY_F2:
                 if path:
                     with open(path, "w") as fh:
                         writetext = "\n".join(text)
@@ -117,9 +145,27 @@ def editor(stdscr, path=None):
                             fh.write(fernet.encrypt(writetext.encode()).decode())
                 break
 
-            elif 32 <= key <= 126:  
+            elif key == curses.KEY_F3:
+                if undo_stack:
+                    redo_stack.append(snapshot())
+                    restore(undo_stack.pop())
+
+            elif key == curses.KEY_F4: 
+                if redo_stack:
+                    undo_stack.append(snapshot())
+                    restore(redo_stack.pop())
+
+            elif 32 <= key <= 126:
+                undo_stack.append(snapshot())
+                redo_stack.clear()
                 text[cursor_y] = text[cursor_y][:cursor_x] + chr(key) + text[cursor_y][cursor_x:]
                 cursor_x += 1
+                if cursor_x >= max_x - 1:
+                    cursor_x = 0
+                    cursor_y += 1
+                    text.insert(cursor_y, "")
+                    if cursor_y - scroll_offset >= max_y - 1:
+                        scroll_offset += 1
 
     except Exception as e:
         print("Editor crashed:", e)
@@ -132,7 +178,7 @@ pswd = input("Enter password: ").encode()
 kdf = PBKDF2HMAC(
     algorithm=hashes.SHA256(),
     length=32,
-    salt=b"",        
+    salt=b"",
     iterations=390000,
 )
 key = base64.urlsafe_b64encode(kdf.derive(pswd))
